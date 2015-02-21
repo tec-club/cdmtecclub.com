29f36c0ac396d67736b388771c840d1b95a0e7bc
Alex Wendland <me@alexwendland.com>
2015-02-20 17:24:12 -0800

    Add floating, networked dot animation to main page header

--- src/render/scripts/main.js
+++ src/render/scripts/main.js
@@ -69,3 +69,130 @@ angular
 			}
 		}
 	}]);
+
+function setupFadeNav() {
+    var delta = 10;
+    var navbar = document.querySelector(".navbar"),
+        body = document.querySelector("body");
+    var lastScrollTop = 0;
+    var didScroll = false;
+    window.addEventListener('scroll', function() {
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var scrollTop = body.scrollTop;
+        if (scrollTop <= navbar.offsetHeight) {
+            DOMUtils.removeClass(navbar, "shadow");
+            DOMUtils.addClass(navbar, "top");
+        } else {
+            DOMUtils.removeClass(navbar, "top");
+            DOMUtils.addClass(navbar, "shadow");
+        }
+        if (Math.abs(scrollTop - lastScrollTop) < delta)
+            return;
+        if (scrollTop < lastScrollTop) {
+            DOMUtils.removeClass(navbar, "hide");
+        } else {
+            if (scrollTop > navbar.offsetHeight) {
+                DOMUtils.addClass(navbar, "hide");
+            }
+        }
+        lastScrollTop = scrollTop;
+    }
+}
+setupFadeNav();
+
+
+
+
+
+var DOMUtils = (function () {
+    var _ = {};
+    
+    _.testParents = function(e, f) {
+        return (f(e) ? true : f(e.parentElement));
+    }
+    
+    _.hasParentClass = function (e, c) {
+        return _.testParents(e, function(el) {
+            return e.classList.contains(c);
+        });
+    }
+    
+    _.removeElem = function (e) {
+        e.parentElement.removeChild(e);
+    }
+    
+    _.getAttr = function (e, a) {
+        return e.attributes.getNamedItem(a).nodeValue;
+    }
+    
+    var getClassList = function (e) {
+        return e.className.split(" ");
+    }
+    
+    var setClassList = function (e, classes) {
+        e.className = classes.join(" ");
+    }
+    
+    _.addClass = function (e, c) {
+        if (e.className.split(" ").indexOf(c) === -1) {
+            e.className = e.className + " " + c;
+        }
+        return e;
+    }
+    
+    _.removeClass = function (e, c) {
+        var classes = getClassList(e);
+        Utils.removeFromList(classes, c);
+        setClassList(e, classes);
+        return e;
+    }
+    
+    _.toggleClass = function (e, c) {
+        var classes = getClassList(e);
+        if (!Utils.removeFromList(classes, c)) {
+            classes.push(c);
+        }
+        setClassList(e, classes);
+        return e;
+    }
+    
+    return _;
+})();
+
+var Utils = (function () {
+    var _ = {};
+    
+    _.forEach = function (a, f) {
+        for (var i = 0; i < a.length; i++) {
+            f(i, a[i]);
+        }
+    }
+    
+    _.forMap = function (o, f) {
+        for (var key in o) {
+            if(o.hasOwnProperty(key)){
+                f(key, o[key]);
+            }
+        }  
+    };
+    
+    _.removeFromList = function (a, e) {
+        var i = a.indexOf(e);
+        if (i === -1)
+            return false;
+        a.splice(i, 1);
+        return true;
+    };
+    
+    return _;
+})();
